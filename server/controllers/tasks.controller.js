9df764d33ef97a154d493fe6ccc238cce9d68b4b
Alvaro Signorio <90202428+Alvaro1251@users.noreply.github.com>
2022-08-11 03:17:05 -0300

    create endpoint

--- server/controllers/tasks.controller.js
+++ server/controllers/tasks.controller.js
@@ -1,15 +1,26 @@
+import {pool} from '../db.js';
+
 export const getTasks = (req, res) => {
-    res.send("obteniendo tareas")
-}
+  res.send("obteniendo tareas");
+};
 export const getTask = (req, res) => {
-    res.send("obteniendo una tarea")
-}
-export const createTask = (req, res) => {
-    res.send("creando tarea")
-}
+  res.send("obteniendo una tarea");
+};
+export const createTask = async (req, res) => {
+  const { title, description } = req.body;
+  const resultado = await pool.query(
+    "INSERT INTO tasks (title, description) VALUES (?,?)",
+    [
+    title,
+    description,
+  ]);
+  console.log(resultado);
+  res.send("creando tarea");
+};
+
 export const updateTask = (req, res) => {
-    res.send("actualizando tarea")
-}
+  res.send("actualizando tarea");
+};
 export const deleteTask = (req, res) => {
-    res.send("eliminando tarea")
-}
+  res.send("eliminando tarea");
+};
